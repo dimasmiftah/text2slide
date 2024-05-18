dcf54951777360cafd56bbda29547d94e4ef5203
dimasmiftah <dimas.mfth@gmail.com>
2024-05-19 03:45:56 +0700

    feat: add text 2 slide mvp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,144 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useState } from 'react';
+
+const TEXT_STORAGE_KEY = 'text';
+const THEME_STORAGE_KEY = 'theme';
+const DEFAULT_TEXT = `Hi, everyone! 👋
+
+My name is Dimas Miftah 🏂
+
+I ship websites 🛩️
+
+https://instagram.com/dimas.mfth`;
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+  const [text, setText] = useState(localStorage?.getItem(TEXT_STORAGE_KEY) || DEFAULT_TEXT);
+  const [slides, setSlides] = useState<string[]>([]);
+  const [activeSlide, setActiveSlide] = useState(0);
+  const [theme, setTheme] = useState<'light' | 'dark'>(localStorage?.getItem(THEME_STORAGE_KEY) as 'light' | 'dark');
+
+  const colorClass = theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-gray-200 text-black';
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+    localStorage.setItem(TEXT_STORAGE_KEY, event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!text) return alert('Please enter some text.');
+
+    setSlides(
+      text
+        .split('\n\n')
+        .map((slide) => slide.trim())
+        .filter(Boolean)
+    );
+  };
+
+  const handleReset = () => {
+    setSlides([]);
+    setActiveSlide(0);
+  };
+
+  const handleNext = () => {
+    setActiveSlide((prev) => (prev < slides.length - 1 ? prev + 1 : prev));
+  };
+
+  const handlePrev = () => {
+    setActiveSlide((prev) => (prev > 0 ? prev - 1 : prev));
+  };
+
+  const handleToggleTheme = () => {
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      localStorage.setItem('theme', newTheme);
+
+      return newTheme;
+    });
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'ArrowLeft') {
+        handlePrev();
+      } else if (event.key === 'ArrowRight') {
+        handleNext();
+      } else if (event.key === 'Escape') {
+        handleReset();
+      } else if (event.key === 'Enter' && event.metaKey) {
+        handleSubmit();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [text, slides, theme]);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', theme === 'dark');
+  }, [theme]);
+
+  const renderThemeToggle = () => (
+    <button className={`w-10 h-10 ${colorClass}`} onClick={handleToggleTheme}>
+      {theme === 'dark' ? '🌚' : '🌞'}
+    </button>
+  );
+
+  return slides.length > 0 ? (
+    <main className='flex flex-col items-center justify-center w-screen h-screen p-48'>
+      <h1 className='text-4xl font-semibold leading-snug'>
+        {slides[activeSlide].includes('http') ? (
+          <a href={slides[activeSlide]} target='_blank' rel='noopener noreferrer'>
+            {slides[activeSlide]}
           </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        ) : (
+          slides[activeSlide]
+        )}
+      </h1>
+      <nav className='flex gap-2 fixed top-4 right-4'>
+        <button className={`w-16 h-10 ${colorClass}`} onClick={handleNext}>
+          {activeSlide + 1} / {slides.length}
+        </button>
+        <button
+          className={`w-10 h-10 ${colorClass}`}
+          onClick={() =>
+            alert(`
+              Use the left (<) and right (>) arrow keys to navigate through the slides.
+              Press the escape (esc) key to exit the presentation.
+            `)
+          }
+        >
+          ?
+        </button>
+        {renderThemeToggle()}
+      </nav>
+    </main>
+  ) : (
+    <main className='flex min-h-screen flex-col items-center justify-center gap-4 p-24'>
+      <h1 className='text-4xl font-bold'>Text 2 Slide</h1>
+      <p className='text-lg'>
+        Convert your text into slides. Separate your slides with <code>two new lines</code>.
+      </p>
+      <form className='flex flex-col gap-4'>
+        <textarea
+          className={`w-full p-4 ${colorClass}`}
+          placeholder='Type your text...'
+          onChange={handleChange}
+          value={text}
+          autoFocus
+          rows={20}
+          cols={80}
+          required
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <button type='button' className={`p-4 ${colorClass}`} onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+      <nav className='flex gap-2 fixed top-4 right-4'>{renderThemeToggle()}</nav>
     </main>
   );
 }
